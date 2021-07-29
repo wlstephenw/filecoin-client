6b2a8cb114160fc5242a995687083317736884a1
stephen <lei.wang@energy-blockchain.com>
2021-07-29 20:01:54 +0800

    add the includsive parameter

--- src/main/java/com/nenglian/filecoin/service/TransferService.java
+++ src/main/java/com/nenglian/filecoin/service/TransferService.java
@@ -1,5 +1,6 @@
 package com.nenglian.filecoin.service;
 
+import com.nenglian.filecoin.rpc.domain.cid.Cid;
 import com.nenglian.filecoin.rpc.domain.exitcode.ExitCode;
 import com.nenglian.filecoin.rpc.domain.types.Message;
 import com.nenglian.filecoin.rpc.domain.types.SignedMessage;
@@ -21,6 +22,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * @author stephen
@@ -48,12 +50,15 @@ public class TransferService {
     public String transfer(Transfer transfer) {
 
         logger.info("receive transfer request:{}", transfer);
-        String res = null;
-
+        if (transfer.getTokenAddress() != null){
+            throw new NotImplementedException();
+        }
         Order orderById = repository.findOrderById(transfer.getReqId());
         if (orderById != null) {
             throw new RuntimeException("duplicated order");
         }
+
+        String res = null;
 
         // 首先落库
         Order order = Order.builder().reqId(transfer.getReqId()).transfer(transfer).status(TransferStatus.PENDING).build();
@@ -62,120 +67,152 @@ public class TransferService {
         Message gasMessage = txm.estimateGas(transfer);
         BigInteger totalFee = BigInteger.valueOf(gasMessage.getGasLimit()).multiply(gasMessage.getGasFeeCap());
         BigInteger balance = wallet.balance(transfer.getFrom());
-        if (balance.compareTo(totalFee) >= 0){
-            // money enough
-            SignedMessage signedMessage = txm.sign(transfer, gasMessage, null);
-            order.setStatus(TransferStatus.GAS_OK);
-            order.setTxId(signedMessage.getMessage().getCid().getStr());
-            repository.save(order);
-            txm.send(signedMessage);
+        BigInteger value = transfer.getValue();
+        BigInteger shortValue = balance.subtract(totalFee.add(value));
 
-            MQTxMessage mq = MQTxMessage.builder()
-                .reqId(order.getReqId())
-                .build();
-            mq.setTransferTxId(signedMessage.getMessage().getCid().getStr());
-            rocketMQTemplate.convertAndSend("filecoin", mq);
+        if (transfer.getFeeInclusive()){
+            // 归集
+            if (balance.compareTo(totalFee) <= 0){
+                throw new RuntimeException(String.format("not enough money, balance:%s, fee:%s", balance, totalFee));
+            }
+            if (balance.subtract(totalFee).compareTo(value) < 0)
+                value = balance.subtract(totalFee);
+
+            transfer.setValue(value);
+            this.transferFund(order, transfer, gasMessage);
 
         }else {
-            Transfer gasTransfer = Transfer.builder()
-                .from(transfer.getGasAddress())
-                .to(transfer.getFrom())
-                .value(gasMessage.getGasFeeCap().multiply(BigInteger.valueOf(gasMessage.getGasLimit())))
-                .speedup(transfer.getSpeedup())
-                .build();
-            SignedMessage sign = txm.sign(gasTransfer, null, null);
-
-            // 先落库
-            order.setGasTxId(sign.getMessage().getCid().getStr());
-            order.setGasMessage(gasMessage);
-            order.setStatus(TransferStatus.PENDING);
-            repository.save(order);
-            txm.send(sign);
-            res = sign.getMessage().getCid().getStr();
+            // 提现
+            if (balance.compareTo(totalFee.add(value)) < 0){
+                throw new RuntimeException(String.format("not enough money, balance:%s, fee:%s, value:%s", balance, totalFee, value));
+            }
+            this.transferFund(order, transfer, gasMessage);
         }
-
-
         return res;
+    }
+
+
+    Cid transferFund(Order order, Transfer transfer, Message gasMessage){
+        MQTxMessage mq = MQTxMessage.builder()
+            .reqId(order.getReqId())
+            .build();
+        SignedMessage signedMessage = txm.sign(transfer, gasMessage, null);
+        order.setStatus(TransferStatus.GAS_OK);
+        order.setTxId(signedMessage.getMessage().getCid().getStr());
+        repository.save(order);
+        try {
+            txm.send(signedMessage);
+            mq.setTransferTxId(signedMessage.getMessage().getCid().getStr());
+        }catch (Exception e){
+            mq.setErrorMsg(e.getMessage());
+        }
+        rocketMQTemplate.convertAndSend("filecoin", mq);
+        return signedMessage.getMessage().getCid();
     }
 
     @EventListener
     public void handleTxEvent(TxReceipt txReceipt) {
-
         if (!wallet.isOurs(txReceipt))
             return;
-
         Order order = repository.findOrderByTxId(txReceipt.getMessage().getCid().getStr());
         Order gasOrder = repository.findOrderByGasTxId(txReceipt.getMessage().getCid().getStr());
         if (order != null) {
-            if (!order.getStatus().equals(TransferStatus.GAS_OK)){
-                logger.info("duplicated tx, cid:{}", txReceipt.getMessage().getCid());
-                return;
-            }
-
-            logger.info("收到transfer交易结果, cid:{}", txReceipt.getMessage().getCid());
-            MQTxMessage mq = MQTxMessage.builder()
-                .reqId(order.getReqId())
-                .chainName("filecoin")
-                .tokenAddress("")
-                .from(txReceipt.getMessage().getFrom())
-                .to(txReceipt.getMessage().getTo())
-                .value(txReceipt.getMessage().getValue())
-                .fee(txReceipt.getInvocResult().getGasCost().getTotalCost())
-                .blockHeight(txReceipt.getBlockHeight())
-                .blockTime(new Date(txReceipt.getBlockTime()))
-                .build();
-
-            if (txReceipt.getReceipt().getExitCode().equals(ExitCode.Ok)) {
-                order.setStatus(TransferStatus.OK);
-                mq.setStatus((byte)2);
-            } else {
-                order.setStatus(TransferStatus.FAIL);
-                mq.setStatus((byte)3);
-            }
-            repository.save(order);
-            rocketMQTemplate.convertAndSend("filecoin", mq);
+            handleTransferResult(txReceipt, order);
         } else if (gasOrder != null) {
-            if (!gasOrder.getStatus().equals(TransferStatus.PENDING)){
-                logger.info("duplicated tx, cid:{}", txReceipt.getMessage().getCid());
-                return;
-            }
-
-            logger.info("收到gas转账交易结果, cid:{}", txReceipt.getMessage().getCid());
-            MQTxMessage mq = MQTxMessage.builder()
-                .reqId(gasOrder.getReqId())
-                .chainName("filecoin")
-                .tokenAddress("")
-                .from(txReceipt.getMessage().getFrom())
-                .to(txReceipt.getMessage().getTo())
-                .value(txReceipt.getMessage().getValue())
-                .fee(txReceipt.getInvocResult().getGasCost().getTotalCost())
-                .blockHeight(txReceipt.getBlockHeight())
-                .blockTime(new Date(txReceipt.getBlockTime()))
-                .build();
-
-            if (txReceipt.getReceipt().getExitCode().equals(ExitCode.Ok)) {
-                gasOrder.setStatus(TransferStatus.GAS_OK);
-                mq.setStatus((byte)2);
-            } else {
-                gasOrder.setStatus(TransferStatus.GAS_FAIL);
-                mq.setStatus((byte)3);
-            }
-            repository.save(gasOrder);
-            if (gasOrder.getStatus().equals(TransferStatus.GAS_FAIL)) {
-                rocketMQTemplate.convertAndSend("filecoin", mq);
-                return;
-            }
-
-            SignedMessage signedMessage = txm.sign(gasOrder.getTransfer(), gasOrder.getGasMessage(), null);
-            gasOrder.setTxId(signedMessage.getMessage().getCid().getStr());
-            repository.save(gasOrder);
-
-            txm.send(signedMessage);
-
-
-            mq.setTransferTxId(signedMessage.getMessage().getCid().getStr());
-            rocketMQTemplate.convertAndSend("filecoin", mq);
+            // 目前不用
+            handleGasTransferResult(txReceipt, gasOrder);
         }
+    }
+
+    private void handleTransferResult(TxReceipt txReceipt, Order order) {
+        if (!order.getStatus().equals(TransferStatus.GAS_OK)){
+            logger.info("duplicated tx, cid:{}", txReceipt.getMessage().getCid());
+            return;
+        }
+
+        logger.info("收到transfer交易结果, cid:{}", txReceipt.getMessage().getCid());
+        MQTxMessage mq = MQTxMessage.builder()
+            .reqId(order.getReqId())
+            .chainName("filecoin")
+            .tokenAddress("")
+            .from(txReceipt.getMessage().getFrom())
+            .to(txReceipt.getMessage().getTo())
+            .value(txReceipt.getMessage().getValue())
+            .fee(txReceipt.getInvocResult().getGasCost().getTotalCost())
+            .blockHeight(txReceipt.getBlockHeight())
+            .blockTime(new Date(txReceipt.getBlockTime()))
+            .build();
+
+        if (txReceipt.getReceipt().getExitCode().equals(ExitCode.Ok)) {
+            order.setStatus(TransferStatus.OK);
+            mq.setStatus((byte)2);
+        } else {
+            order.setStatus(TransferStatus.FAIL);
+            mq.setStatus((byte)3);
+        }
+        repository.save(order);
+        rocketMQTemplate.convertAndSend("filecoin", mq);
+    }
+
+
+    Cid transferGas(Order order, Transfer transfer, Message gasMessage){
+        Transfer gasTransfer = Transfer.builder()
+            .from(transfer.getGasAddress())
+            .to(transfer.getFrom())
+            .value(gasMessage.getGasFeeCap().multiply(BigInteger.valueOf(gasMessage.getGasLimit())))
+            .speedup(transfer.getSpeedup())
+            .build();
+        SignedMessage sign = txm.sign(gasTransfer, null, null);
+
+        // 先落库
+        order.setGasTxId(sign.getMessage().getCid().getStr());
+        order.setGasMessage(gasMessage);
+        order.setStatus(TransferStatus.PENDING);
+        repository.save(order);
+        txm.send(sign);
+        return sign.getMessage().getCid();
+    }
+
+    private void handleGasTransferResult(TxReceipt txReceipt, Order gasOrder) {
+        if (!gasOrder.getStatus().equals(TransferStatus.PENDING)){
+            logger.info("duplicated tx, cid:{}", txReceipt.getMessage().getCid());
+            return;
+        }
+
+        logger.info("收到gas转账交易结果, cid:{}", txReceipt.getMessage().getCid());
+        MQTxMessage mq = MQTxMessage.builder()
+            .reqId(gasOrder.getReqId())
+            .chainName("filecoin")
+            .tokenAddress("")
+            .from(txReceipt.getMessage().getFrom())
+            .to(txReceipt.getMessage().getTo())
+            .value(txReceipt.getMessage().getValue())
+            .fee(txReceipt.getInvocResult().getGasCost().getTotalCost())
+            .blockHeight(txReceipt.getBlockHeight())
+            .blockTime(new Date(txReceipt.getBlockTime()))
+            .build();
+
+        if (txReceipt.getReceipt().getExitCode().equals(ExitCode.Ok)) {
+            gasOrder.setStatus(TransferStatus.GAS_OK);
+            mq.setStatus((byte)2);
+        } else {
+            gasOrder.setStatus(TransferStatus.GAS_FAIL);
+            mq.setStatus((byte)3);
+        }
+        repository.save(gasOrder);
+        if (gasOrder.getStatus().equals(TransferStatus.GAS_FAIL)) {
+            rocketMQTemplate.convertAndSend("filecoin", mq);
+            return;
+        }
+
+        SignedMessage signedMessage = txm.sign(gasOrder.getTransfer(), gasOrder.getGasMessage(), null);
+        gasOrder.setTxId(signedMessage.getMessage().getCid().getStr());
+        repository.save(gasOrder);
+
+        txm.send(signedMessage);
+
+        mq.setTransferTxId(signedMessage.getMessage().getCid().getStr());
+        rocketMQTemplate.convertAndSend("filecoin", mq);
     }
 
 }
